c2188f79627abe92827e906cf00f2891040921f0
Amit Choukroun <amit@mtrn.co.il>
2020-02-28 09:11:33 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+const RESTPORT = process.env.RESTPORT || '8080'; 
+const _ = require('lodash');
+var cors = require('cors');
+
+module.exports = (data) => (exec) => {
+  const restRoutes = data.routes;
+  
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  app.use(cors());
+
+  _.each(restRoutes, (flowName, routeName) => {
+    const [method, url] = routeName.split(' ');
+
+    app[method](url, async (req, res) => {
+      const response = await exec(flowName, {headers: req.headers, body: req.body, params: req.params, query: req.query});
+      if(response.file) {
+        return res.sendFile(response.file);
+      } 
+      
+      res.send(response);
+    });
+  });
+
+  app.listen(RESTPORT, () => {
+    console.log(`${new Date()} rest server is listening on port ${RESTPORT}`);
+  });
+}
