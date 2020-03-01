c6224d6d0cf730131c0c06c7b0e87f3a1910cd4a
Amit Choukroun <amit@mtrn.co.il>
2020-03-01 12:03:15 +0200

    chore: add compresion

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const bodyParser = require('body-parser')
 const RESTPORT = process.env.RESTPORT || '8080'; 
 const _ = require('lodash');
-var cors = require('cors');
+const cors = require('cors');
+const compression = require('compression')
 
 module.exports = (data) => (exec) => {
   const restRoutes = data.routes;
@@ -11,6 +12,7 @@ module.exports = (data) => (exec) => {
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
   app.use(cors());
+  app.use(compression({level: 1}))
 
   _.each(restRoutes, (flowName, routeName) => {
     const [method, url] = routeName.split(' ');
