f9be9c337d32ead00523d20342910e4f26ba30f8
Amit Choukroun <amit@mtrn.co.il>
2020-05-20 18:06:48 +0300

    chore: fix express routes

--- index.js
+++ index.js
@@ -2,29 +2,33 @@ const express = require('express');
 const app = express();
 const _ = require('lodash');
 
-function awsRouteRegister(flowName, exec, req, res) {
+async function awsRouteRegister(flowName, exec, req, res) {
   const response = await exec(flowName, {headers: req.headers, body: req.body, pathParameters: req.params, queryStringParameters: req.query})
   .catch(error => ({statusCode: 500, body:{error: error.message}}));
 
   res.status(response.statusCode).send(response.body);
 }
 
-function standardRouteRegister(flowName, exec, req, res) {
+async function standardRouteRegister(flowName, exec, req, res) {
   const response = await exec(flowName, {headers: req.headers, body: req.body, params: req.params, query: req.query});
   
   res.send(response);
 }
 
-module.exports = (data = {isAwsRoutes: false, port: 8080, routes: {}}) => (exec) => {
+module.exports = (data = {isAwsRoutes: false, port: 8080, routes: {}, middlewares: []}) => (exec) => {
   const restRoutes = data.routes;
+
+  _.each(data.middlewares, middleware => {
+    app.use(middleware);
+  })
 
   _.each(restRoutes, (flowName, routeName) => {
     const [method, url] = routeName.split(' ');
 
     app[method](url, async (req, res) => {
       data.isAwsRoutes 
-        ? awsRouteRegister(flowName, exec, req, res)
-        : standardRouteRegister(flowName, exec, req, res);
+        ? await awsRouteRegister(flowName, exec, req, res)
+        : await standardRouteRegister(flowName, exec, req, res);
     });
   });
 
