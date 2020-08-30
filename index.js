205ca9754e14c4ee278ded26b0c5efd3b8e746db
Amit Choukroun <amit@mtrn.co.il>
2020-08-30 14:13:46 +0300

    chore: support statefull object on initialize

--- index.js
+++ index.js
@@ -2,20 +2,22 @@ const express = require('express');
 const app = express();
 const _ = require('lodash');
 
-async function awsRouteRegister(flowName, exec, req, res) {
-  const response = await exec(flowName, {headers: req.headers, body: req.body, pathParameters: req.params, queryStringParameters: req.query})
-  .catch(error => ({statusCode: 500, body:{error: error.message}}));
+async function awsRouteRegister(flowName, exec, statefulObj, req, res) {
+  const response = 
+    await exec(flowName, {
+      headers: req.headers, body: req.body, pathParameters: req.params, queryStringParameters: req.query}, {res, req, ...statefulObj})
+    .catch(error => ({statusCode: 500, body:{error: error.message}}));
 
   res.status(response.statusCode).send(response.body);
 }
 
-async function standardRouteRegister(flowName, exec, req, res) {
-  const response = await exec(flowName, {headers: req.headers, body: req.body, params: req.params, query: req.query});
+async function standardRouteRegister(flowName, exec, statefulObj, req, res) {
+  const response = await exec(flowName, {headers: req.headers, body: req.body, params: req.params, query: req.query}, {res, req, ...statefulObj});
   
   res.send(response);
 }
 
-module.exports = (data = {isAwsRoutes: false, port: 8080, routes: {}, middlewares: []}) => (exec) => {
+module.exports = (data = {isAwsRoutes: false, port: 8080, routes: {}, middlewares: [], statefulObj: {}}) => (exec) => {
   const restRoutes = data.routes;
 
   _.each(data.middlewares, middleware => {
@@ -27,8 +29,8 @@ module.exports = (data = {isAwsRoutes: false, port: 8080, routes: {}, middleware
 
     app[method](url, async (req, res) => {
       data.isAwsRoutes 
-        ? await awsRouteRegister(flowName, exec, req, res)
-        : await standardRouteRegister(flowName, exec, req, res);
+        ? await awsRouteRegister(flowName, exec, data.statefulObj, req, res)
+        : await standardRouteRegister(flowName, exec, data.statefulObj, req, res);
     });
   });
 
